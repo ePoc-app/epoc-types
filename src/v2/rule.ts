a5ba071f37736b019aa325fe88ad0f4e87eda22d
ROSPARS Benoit <benoit.rospars@inria.fr>
2023-07-19 09:55:33 +0000

    Fix operands to supports boolean and more than 2 operands

--- src/v2/rule.ts
+++ src/v2/rule.ts
@@ -1,7 +1,7 @@
 // Types to represents jsonlogic rules https://jsonlogic.com/
 export type Operators = "var" | ">" | ">=" | "<" | "<=" | "===" | "!==" | "and" | "or";
-export type Operand = number | string | Rule;
-export type Operands = [Operand, Operand?];
+export type Operand = number | string | boolean | Rule;
+export type Operands = string | [Operand, Operand, ...Operand[]]; // minimum 2 operands or a value if operator is var
 
 export type Rule = {
   [key in Operators]?: Operands;
